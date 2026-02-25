cc3acb54ebeea0a3ece7c2c8aa60649df96d053a
helosvflx <heloisasevf@gmail.com>
2026-02-25 14:33:38 -0300

    Add files via upload

---
+++ UC - Algoritmo 25-02-2026 python/atv-variaveis.py
@@ -0,0 +1,5 @@
+var1 = "Helô"
+Var1 = 17
+
+print(var1)
+print(Var1)
---
+++ UC - Algoritmo 25-02-2026 python/inicio.py
@@ -0,0 +1,10 @@
+nome = "Ana"
+idade = 17
+altura = 1.65
+matriculado = True
+nome = "Helô"
+
+print(nome)
+print(idade)
+print(altura)
+print(matriculado)
